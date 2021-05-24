f0ccfffbd221a05791b564bf9d311b81df4b8989
daniel-rickman <daniel.rickman@noxcrew.com>
2021-05-24 23:48:59 +0100

    Add main class and plugin.yml

---
+++ src/main/kotlin/dev/dancr/circuit/Circuit.kt
@@ -0,0 +1,17 @@
+package dev.dancr.circuit
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class Circuit : JavaPlugin() {
+
+    @Override
+    override fun onEnable() {
+        //
+    }
+
+    @Override
+    override fun onDisable() {
+        //
+    }
+
+}
